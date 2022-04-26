f18d53a876abd147dd2740493afca77cd7ca4372
Antonis Ioannou <antonios.ioannou@protonmail.com>
2022-04-26 17:01:54 +0300

    updates

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace asp.net_core_5_InAndOut.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+    }
+}
