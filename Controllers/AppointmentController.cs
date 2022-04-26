31a7e209d3c878cfd4d20a1b22d93cc6c9c138a8
Antonis Ioannou <antonios.ioannou@protonmail.com>
2022-04-26 14:52:21 +0300

    Add project files.

---
+++ Controllers/AppointmentController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace asp.net_core_5_InAndOut.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
