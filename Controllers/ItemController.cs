9b2deacd52e1300b431d617fba1e417fab7b8cf9
Antonis Ioannou <antonios.ioannou@protonmail.com>
2022-04-27 11:14:05 +0300

    update

---
+++ Controllers/ItemController.cs
@@ -0,0 +1,26 @@
+﻿using asp.net_core_5_InAndOut.Data;
+using asp.net_core_5_InAndOut.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace asp.net_core_5_InAndOut.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ItemController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Item> objList = _db.Item;
+            return View(objList);
+        }
+    }
+}
